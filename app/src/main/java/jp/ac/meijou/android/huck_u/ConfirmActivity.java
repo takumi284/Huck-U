0c6dc8781ff64dd9ce4d0777d200a1785ff7d638
Takumi Hirabayashi <221205139@ccmailg.meijo-u.ac.jp>
2023-12-05 11:10:30 +0900

    カメラでqrコードを読み取ると，画面遷移するように変更

--- app/src/main/java/jp/ac/meijou/android/huck_u/ConfirmActivity.java
+++ app/src/main/java/jp/ac/meijou/android/huck_u/ConfirmActivity.java
@@ -3,12 +3,30 @@ package jp.ac.meijou.android.huck_u;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import jp.ac.meijou.android.huck_u.databinding.ActivityConfirmBinding;
+import jp.ac.meijou.android.huck_u.databinding.ActivityViewBinding;
 
 public class ConfirmActivity extends AppCompatActivity {
+
+    private ActivityConfirmBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_confirm);
+        binding = ActivityConfirmBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // IntentからQRコードの情報を取得
+        String qrCodeValue = getIntent().getStringExtra("QRCodeValue");
+
+        // qrCodeValue を使用して必要な処理を行う
+        if (qrCodeValue != null) {
+            // qrCodeValue を使用して何か処理を行う
+            binding.editSchoolNumber.setText(qrCodeValue);
+        } else {
+            Log.e("AnotherActivity", "QR Code Value is null");
+        }
     }
 }
