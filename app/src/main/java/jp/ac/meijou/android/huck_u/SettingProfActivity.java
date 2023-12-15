de0dbeafff46479749c59c4dc9d1399cdd8bf0ff
Takumi Hirabayashi <221205139@ccmailg.meijo-u.ac.jp>
2023-12-15 14:12:21 +0900

    firebaseからの読み込みを追加

--- app/src/main/java/jp/ac/meijou/android/huck_u/SettingProfActivity.java
+++ app/src/main/java/jp/ac/meijou/android/huck_u/SettingProfActivity.java
@@ -10,6 +10,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
@@ -26,6 +27,7 @@ public class SettingProfActivity extends AppCompatActivity {
         ImageView selectedImageView = findViewById(R.id.selectedImageView);
         selectedImageView.setImageResource(R.drawable.icon_sample);
         ImageButton backButton = findViewById(R.id.backButton);
+        EditText editTextText = findViewById(R.id.editTextText);
 
         selectImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,6 +39,7 @@ public class SettingProfActivity extends AppCompatActivity {
         //読み取り
         SharedPreferences preferences = getSharedPreferences("MyPreferences", Context.MODE_PRIVATE);
         int confirmed = preferences.getInt("confirmed", 0);
+        String name = preferences.getString("userName", "");
 
         backButton.setOnClickListener(view -> {
             Intent intent;
@@ -50,6 +53,8 @@ public class SettingProfActivity extends AppCompatActivity {
             startActivity(intent);
             finish();
         });
+
+        editTextText.setText(name);
     }
     private void openGallery() {
         Intent galleryIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
